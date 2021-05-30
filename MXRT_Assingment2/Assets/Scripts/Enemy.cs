ed08891c610f51aaf3560630d93d574cdf3c82e8
Minalinsky02 <74691266+Minalinsky02@users.noreply.github.com>
2021-05-30 19:39:01 +0800

    Working UI touch interface for AR object

--- MXRT_Assingment2/Assets/Scripts/Enemy.cs
+++ MXRT_Assingment2/Assets/Scripts/Enemy.cs
@@ -8,9 +8,14 @@ public class Enemy : MonoBehaviour
     public GameObject enemyUI;
     public GameObject enemyUICorrect;
     public GameObject enemyUIWrong;
+    public GameObject enemy;
     public static bool enemyUIPop = false;
     public static bool enemyUICorrectPop = false;
     public static bool enemyUIWrongPop = false;
+
+    [SerializeField]
+    private Camera arCamera;
+    
 
 
     // Start is called before the first frame update
@@ -22,15 +27,36 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Camera.main.transform);
+        enemyPopUp();
     }
 
-    public void enemyPopUp()
+    void enemyPopUp()
     {
-        enemyUI.SetActive(true);
-        Time.timeScale = 0f;
-        enemyUIPop = true;
-        Debug.Log("You tapped on enemy");
+
+        if (Input.touchCount == 0)
+            return;
+
+
+        transform.LookAt(Camera.main.transform);
+
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            var ray = arCamera.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit) && hit.transform.name == "Enemy")
+            {
+                if (hit.collider != null)
+                {
+                    Instantiate(enemyUI, enemy.transform.position, Quaternion.identity);
+                    enemyUI.SetActive(true);
+                    Time.timeScale = 0f;
+                    enemyUIPop = true;
+                }
+            }
+        }
     }
+
+ 
 
 }
